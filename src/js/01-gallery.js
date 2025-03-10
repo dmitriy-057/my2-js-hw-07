10e992e556b2e74fc2c87adcf80bd3b981a94084
Dmitriy Gonharov <my.webpage057@gmail.com>
2025-03-10 23:54:59 +0200

    add galleryMarkup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+
+function createGalleryMarkup(items) {
+    const markup = items.map(({preview, original,description}) => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>
+    `
+    }).join('');
+    return markup;
+};
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const isGalleryImageEl = evt.target.classList.contains("gallery__image");
+    if(!isGalleryImageEl) {
+        return;
+    }
+    console.log(evt.target);
+    openModal(evt.target.dataset.source);
+ 
+};
+
+function openModal(arg) {
+    const instance = basicLightbox.create(`
+    <img src=${arg}>
+    `);
+    instance.show(() => console.log('lightbox now visible'))
+
+};
+
